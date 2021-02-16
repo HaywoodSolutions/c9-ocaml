184a76debf1d92225b4ce5155ba7b6eb79391f99
Thomas Haywood <thomashaywoodproductions@gmail.com>
2021-02-16 20:52:26 +0000

    Added Custom Changes

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 c9-ocaml
 ========
 
-This repo contains a script to install [OCaml](http://ocaml.org/) and [OPAM](http://opam.ocamlpro.com/) inside [Cloud9 IDE](https://c9.io/) workspaces, enabling collaborative editing, compiling, and testing of OCaml programs "in the cloud."
+This repo contains a script to install [OCaml](http://ocaml.org/) and [OPAM](http://opam.ocamlpro.com/) inside [Cloud9 IDE](https://c9.io/) workspaces, enabling collaborative editing, compiling, and testing of OCaml programs "in the cloud." Also installs ocamlfind and ocamlbuild.
 
 In any workspace, enter into the terminal pane:
 
 ````
-curl -L https://raw.github.com/mlin/c9-ocaml/master/c9-ocaml.sh | bash -ex
+curl -L https://raw.github.com/HaywoodSolutions/c9-ocaml/master/c9-ocaml.sh | bash -ex
 eval $(opam config env)
+git clone https://git.cs.kent.ac.uk/sao/example-compiler.git
 ````
 
 The OCaml toolchain and OPAM will then be ready for use!
--- c9-ocaml.sh
+++ c9-ocaml.sh
@@ -2,7 +2,7 @@
 # Install OCaml and OPAM inside a Cloud9 IDE workspace.
 # Run this script inside the workspace terminal, then eval $(opam config env)
 
-OCAML_URL=http://caml.inria.fr/pub/distrib/ocaml-4.02/ocaml-4.02.3.tar.gz
+OCAML_URL=http://caml.inria.fr/pub/distrib/ocaml-4.07/ocaml-4.07.0.tar.gz
 
 # create a scratch directory
 D=$(mktemp --tmpdir -d c9-ocaml-XXX)
@@ -19,16 +19,15 @@ cd ocaml-*
 make world
 make install
 
-# build & install native-code ocaml (but not native-code camlp4, which doesn't
-# build inside the c9 workspace for unknown reasons, perhaps RAM limits?)
-./configure -no-camlp4 -no-graph -no-curses -prefix $HOME
-make world.opt
-make install
-
 # download & install opam
-wget -c http://www.ocamlpro.com/pub/opam_installer.sh
+wget https://raw.githubusercontent.com/ocaml/opam/master/shell/opam_installer.sh
 yes '' | sh ./opam_installer.sh $HOME/bin system
 
 # clean up
 popd
 rm -rf $D
+
+opam update
+opam upgrade
+opam install ocamlfind
+opam install ocamlbuild
